d8a3326078cfb021f64e555e7623bd939d6b513d
jm <jm@macbook.local>
2020-12-19 15:26:02 -0500

    fix nodemon

--- src/api/v1/helloworld/controller.ts
+++ src/api/v1/helloworld/controller.ts
@@ -2,10 +2,10 @@ import { ApiRequest, ApiResponse } from './model';
 import { Body, Controller, Post, Route } from 'tsoa';
 
 @Route('helloworld')
-export class UsersController extends Controller {
+export class HelloWorldController extends Controller {
   @Post()
   public async createUser(@Body() request: ApiRequest): Promise<ApiResponse> {
-    console.log(request);
-    return { uuid: '0000-0000-0000-0000', message: `Hello ${request.name}` };
+    console.log('☢️', request);
+    return { uuid: '0000-0000-0000-0000', message: `Hello ${request.name} Hola` };
   }
 }
