efb8f4b8b04dac7b6c85e798feae999435a7a03a
jm <jm@macbook.local>
2020-12-19 15:15:46 -0500

    added scripts

--- src/api/v1/controller.ts
+++ src/api/v1/controller.ts
@@ -1,14 +1,11 @@
+import { ApiRequest, ApiResponse } from './model';
+import { Body, Controller, Post, Route } from 'tsoa';
 
-import { ApiRequest, ApiResponse } from "./model";
-import { Body, Controller, Post, Route } from "tsoa";
-
-@Route("helloworld")
+@Route('helloworld')
 export class UsersController extends Controller {
   @Post()
-  public async createUser(
-    @Body() request: ApiRequest
-  ): Promise<ApiResponse> {
+  public async createUser(@Body() request: ApiRequest): Promise<ApiResponse> {
     console.log(request);
-    return { uuid: "0000-0000-0000-0000", message: `Hello ${request.name}` };
+    return { uuid: '0000-0000-0000-0000', message: `Hello ${request.name}` };
   }
 }
